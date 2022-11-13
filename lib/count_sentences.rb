296205d538afbd5f44c6dbfb2890a03f64e46d6d
Natasha <natasha.khoe@gmail.com>
2022-11-13 12:03:30 +1100

    commit tree ce4fc20b3e4fbbbef3d28be1295ef646eddc6075

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    start_with_conjuction = self.start_with?("But ", "And ", "Or ", "Nor ")
+    end_with_period = self.end_with?(".")
+
+    if start_with_conjuction
+      false
+    else
+      if end_with_period
+        true
+      else
+        false
+      end
+    end
+
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.?!]+/).length
   end
 end
